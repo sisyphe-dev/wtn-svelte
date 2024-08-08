8e51d3bbe68b1595e816e9c9ea4dc2474936aec9
uk4z <ulyssekaz@gmail.com>
2024-08-08 16:26:19 +0200

    Fix refactor

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -2,6 +2,10 @@ import { writable } from 'svelte/store';
 import { type User, Canisters, WaterNeuronInfo } from './state';
 import { Asset, AssetType } from '$lib';
 import { Toast } from './toast';
+import BigNumber from 'bignumber.js';
+import { get } from 'svelte/store';
+import { Principal } from '@dfinity/principal';
+import { encodeIcrcAccount } from '@dfinity/ledger-icrc';
 
 /* === Flags === */
 export const isLogging = writable<boolean>(false);
@@ -15,15 +19,68 @@ export const inSnsMenu = writable<boolean>(false);
 /* === Components === */
 export const language = writable<'en' | 'es' | 'ja' | 'ru'>('en');
 export const selectedAsset = writable<Asset>(new Asset(AssetType.ICP));
-export const selectedSns = writable<string>('BOOM DAO');
-export const snsPrincipal = writable<string>('xomae-vyaaa-aaaaq-aabhq-cai');
-export const encodedSnsIcrcAccount = writable<string>('-/-');
 export const user = writable<User | undefined>(undefined);
 export const canisters = writable<Canisters | undefined>(undefined);
 export const waterNeuronInfo = writable<WaterNeuronInfo | undefined>(undefined);
 
+/* === SNS === */
+function createSns() {
+	const { subscribe, set, update } = writable<{
+		name: string;
+		principal: string;
+		encodedAccount: string;
+		icpBalance: BigNumber | undefined;
+		nicpBalance: BigNumber | undefined;
+	}>({
+		name: '',
+		principal: '',
+		encodedAccount: '-/-',
+		icpBalance: undefined,
+		nicpBalance: undefined
+	});
+
+	return {
+		subscribe,
+		setPrincipal: (principal: string) => update((sns) => ({ ...sns, principal })),
+		setName: (name: string) => update((sns) => ({ ...sns, name })),
+		setEncodedAccount: (encodedAccount: string) => update((sns) => ({ ...sns, encodedAccount })),
+		setIcpBalance: (icpBalance: BigNumber) => update((sns) => ({ ...sns, icpBalance })),
+		setNicpBalance: (nicpBalance: BigNumber) => update((sns) => ({ ...sns, nicpBalance })),
+		reset: () =>
+			set({
+				name: '',
+				principal: '',
+				encodedAccount: '-/-',
+				icpBalance: undefined,
+				nicpBalance: undefined
+			})
+	};
+}
+export const sns = createSns();
+
+export async function handleSnsChange(name?: string, principal?: string) {
+	const boomerang = get(canisters)?.boomerang;
+	if (!boomerang) return;
+
+	sns.reset();
+
+	if (name && principal) {
+		sns.setName(name);
+		sns.setPrincipal(principal);
+		const account = await boomerang.get_staking_account(Principal.fromText(principal));
+		const hex = encodeIcrcAccount({
+			owner: account.owner,
+			subaccount: account.subaccount[0]
+		});
+		sns.setEncodedAccount(hex);
+	} else {
+		sns.setName('Custom');
+		sns.setPrincipal('');
+	}
+}
+
 /* === Toasts === */
-function creatToasts() {
+function createToasts() {
 	const { subscribe, set, update } = writable<Toast[]>([]);
 
 	return {
@@ -34,7 +91,7 @@ function creatToasts() {
 	};
 }
 
-export const toasts = creatToasts();
+export const toasts = createToasts();
 
 /* === Input Value ==== */
 function createInputValue() {
