37e924e1d337786742c99aa83ee2d4f38650c8e7
uk4z <ulyssekaz@gmail.com>
2024-08-18 11:34:37 +0200

    Faq on a separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,6 @@
 	import { fade, slide } from 'svelte/transition';
 
 	export let data;
-
-	function getContent(language: string) {
-		switch (language) {
-			case 'en':
-				return data.en.sections;
-			case 'es':
-				return data.es.sections;
-			case 'ru':
-				return data.ru.sections;
-			case 'ja':
-				return data.ja.sections;
-			default:
-				return [];
-		}
-	}
-
-	let toggledMap = getContent($language).map(() => {
-		return false;
-	});
 </script>
 
 <StatsWidget />
