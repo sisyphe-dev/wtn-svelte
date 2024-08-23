5f38e4ce6d09aa2f2620ea1fdaa891d14b359cf4
Your Name <you@example.com>
2024-08-23 10:34:33 +0200

    fix number display

--- tests/lib.spec.ts
+++ tests/lib.spec.ts
@@ -120,9 +120,10 @@ test('computeRewards', () => {
 });
 
 test('Display US-format', () => {
-	expect(displayUsFormat(BigNumber(1_000_000.0123942))).toBe("1'000'000.0124");
-	expect(displayUsFormat(BigNumber(1_000_000.018942))).toBe("1'000'000.0189");
-	expect(displayUsFormat(BigNumber(1_000_000.0123942), 8)).toBe("1'000'000.0123942");
+	expect(displayUsFormat(BigNumber(1_000_000.0123942), 4)).toBe("1'000'000.0123");
+	expect(displayUsFormat(BigNumber(1_000_000.018942), 4)).toBe("1'000'000.0189");
+	expect(displayUsFormat(BigNumber(1_000_000.0123942), 8)).toBe("1'000'000.01239420");
+	expect(displayUsFormat(BigNumber(9.9992))).toBe('9.99');
 });
 
 test('Test truncated format for principal', () => {
