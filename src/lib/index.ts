7457ff522018cc60204def29d72bb060f9ec77db
uk4z <ulyssekaz@gmail.com>
2024-10-28 15:21:23 +0100

    unused imports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,8 +3,7 @@ import BigNumber from 'bignumber.js';
 import type { WithdrawalStatus } from '../declarations/water_neuron/water_neuron.did';
 import { AccountIdentifier } from '@dfinity/ledger-icp';
 import { decodeIcrcAccount } from '@dfinity/ledger-icrc';
-import type { Account, TransferArg, TransferArgs } from '../declarations/icp_ledger/icp_ledger.did';
-import { IDL } from '@dfinity/candid';
+import type { Account } from '../declarations/icp_ledger/icp_ledger.did';
 
 export const E8S = BigNumber(10).pow(BigNumber(8));
 
