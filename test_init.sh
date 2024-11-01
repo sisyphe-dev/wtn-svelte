e6cd0605655717083378cc39df28566e1e63e353
uk4z <ulyssekaz@gmail.com>
2024-11-01 12:02:18 +0100

    waterneuron canister update on test_init

--- test_init.sh
+++ test_init.sh
@@ -8,6 +8,6 @@ intermediary=$( (dfx ledger account-id))
 dfx identity use icp-ident-RqOPnjj5ERjAEnwlvfKw
 dfx ledger transfer --memo 0 --icp 1_000_000 $intermediary
 dfx identity use default
-dfx canister call nns-ledger icrc2_approve '(record {spender= record{owner=principal "n76cn-tyaaa-aaaam-acc5a-cai";}; amount=100_000_000_000_000;})'
+dfx canister call nns-ledger icrc2_approve '(record {spender= record{owner=principal "tsbvt-pyaaa-aaaar-qafva-cai";}; amount=100_000_000_000_000;})'
 dfx canister call water_neuron icp_to_nicp '(record{amount_e8s=10_000_000_000_000})'
 dfx canister call nicp_ledger icrc1_transfer '(record{to=record{owner=principal "syna7-6ipnd-myx4g-ia46u-nxwok-u5nrr-yxgpi-iang7-lvru2-i7n23-tqe"}; amount=9_000_000_000_000;})'
