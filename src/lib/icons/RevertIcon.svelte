5cf6f5bc8a7a5bb7111a0092b5f098753e9c93f5
uk4z <ulyssekaz@gmail.com>
2024-11-07 17:22:43 +0100

    revericon.svelte

---
+++ src/lib/icons/RevertIcon.svelte
@@ -0,0 +1,23 @@
+<svg
+	viewBox="-2.4 -2.4 28.80 28.80"
+	fill="none"
+	xmlns="http://www.w3.org/2000/svg"
+	transform="rotate(90)matrix(1, 0, 0, 1, 0, 0)"
+	height="30px"
+	width="30px"
+	><g id="SVGRepo_bgCarrier" stroke-width="1"></g><g
+		id="SVGRepo_tracerCarrier"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke="var(--main-color)"
+		stroke-width="0.144"
+	></g><g id="SVGRepo_iconCarrier">
+		<path
+			d="M12.5 20.5C17.1944 20.5 21 16.6944 21 12C21 7.30558 17.1944 3.5 12.5 3.5C7.80558 3.5 4 7.30558 4 12C4 13.5433 4.41128 14.9905 5.13022 16.238M1.5 15L5.13022 16.238M6.82531 12.3832L5.47107 16.3542L5.13022 16.238"
+			stroke="var(--main-color)"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		></path>
+	</g></svg
+>
